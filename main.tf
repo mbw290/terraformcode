a58eeefa5dab3682de0a4f65789cfcae0eefcfb3
Matthew Wimpelberg <mwimpelberg@gmail.com>
2020-06-01 15:36:27 -0400

    updating to use variables and loops

--- main.tf
+++ main.tf
@@ -1,31 +1,23 @@
 provider "aws" {
-  region = "us-west-2"
-}
-
-variable "public_ip" {
-  type = string
+  region  = var.region
+  profile = var.profile
 }
 
 resource "aws_vpc" "mw" {
-  cidr_block       = "172.16.0.0/16"
+  cidr_block       = var.vpccidr
   instance_tenancy = "default"
 }
 
-resource "aws_subnet" "public" {
-  vpc_id            = "${aws_vpc.mw.id}"
-  availability_zone = "us-west-2a"
-  cidr_block        = "172.16.0.0/24"
-}
-resource "aws_subnet" "private" {
-  vpc_id            = "${aws_vpc.mw.id}"
-  availability_zone = "us-west-2a"
-  cidr_block        = "172.16.1.0/24"
+resource "aws_subnet" "mattssubnets" {
+  for_each = var.mysubnets
+  vpc_id     = "${aws_vpc.mw.id}"
+  availability_zone = var.availability_zone
+  cidr_block = each.value
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.mw.id}"
 }
-
 
 resource "aws_eip" "byoip-ip" {
   vpc              = true
@@ -33,13 +25,13 @@ resource "aws_eip" "byoip-ip" {
 
 resource "aws_nat_gateway" "ngw" {
   allocation_id = "${aws_eip.byoip-ip.id}"
-  subnet_id     = "${aws_subnet.public.id}"
+  subnet_id     = "${aws_subnet.mattssubnets["public"].id}"
 }
 
 resource "aws_route_table" "prir" {
   vpc_id = "${aws_vpc.mw.id}"
   route {
-    cidr_block = "192.168.1.0/24"
+    cidr_block = var.home_cidr
     gateway_id = "${aws_vpn_gateway.vpn_gateway.id}"
   }
   route {
@@ -57,12 +49,12 @@ resource "aws_route_table" "pubr" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = "${aws_subnet.private.id}"
+  subnet_id      = "${aws_subnet.mattssubnets["private"].id}"
   route_table_id = "${aws_route_table.prir.id}"
 }
 
 resource "aws_route_table_association" "b" {
-  subnet_id      = "${aws_subnet.public.id}"
+  subnet_id      = "${aws_subnet.mattssubnets["public"].id}"
   route_table_id = "${aws_route_table.pubr.id}"
 }
 
@@ -88,19 +80,13 @@ resource "aws_vpn_connection_route" "home" {
   vpn_connection_id      = "${aws_vpn_connection.main.id}"
 }
 
-resource "aws_instance" "mattec2" {
-  ami = "ami-0d6621c01e8c2de2c"
+resource "aws_instance" "mattsec2"{
+  for_each = var.amis
+  ami      = each.value
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.private.id}"
-  key_name = "mw-keys"
-  vpc_security_group_ids = "${aws_security_group.allow_all.*.id}"
-}
+  key_name = var.ec2_keys
+  subnet_id      = "${aws_subnet.mattssubnets["private"].id}"
 
-resource "aws_instance" "mattrhel8" {
-  ami = "ami-02f147dfb8be58a10s"
-  instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.private.id}"
-  key_name = "mw-keys"
   vpc_security_group_ids = "${aws_security_group.allow_all.*.id}"
 }
 
@@ -124,4 +110,20 @@ resource "aws_security_group" "allow_all" {
     cidr_blocks = ["0.0.0.0/0"]
  
   }
+}
+
+output rhel8ip {
+  value = "${aws_instance.mattsec2["rhel8"].private_ip}"
+}
+
+output al2ip {
+  value = "${aws_instance.mattsec2["al2"].private_ip}"
+}
+
+output cg {
+  value = "${aws_customer_gateway.customer_gateway.id}"
+}
+
+output pubip {
+  value = "${aws_nat_gateway.ngw.public_ip}"
 }
